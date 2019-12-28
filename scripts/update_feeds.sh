5e01e57ac503546ae763856bea09351901192c41
caiyy <yylcy@vip.qq.com>
2019-12-29 06:38:46 +0800

    Update update_feeds.sh

--- scripts/update_feeds.sh
+++ scripts/update_feeds.sh
@@ -16,8 +16,6 @@ cd openwrt
 
 mkdir -p package/z-last-build-packages || true
 cd package/z-last-build-packages
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/tty228/luci-app-serverchan
 # install_package PACKAGE_DIR GIT_URL
 install_package() {
   if (( $# != 2 )); then
@@ -35,3 +33,6 @@ install_package() {
 # Note that to have it compiled, you also have to set its CONFIG_* options
 # Example:
 # install_package mentohust https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git
+
+install_package mentohust https://github.com/jerrykuku/luci-theme-argon.git
+install_package mentohust https://github.com/tty228/luci-app-serverchan
